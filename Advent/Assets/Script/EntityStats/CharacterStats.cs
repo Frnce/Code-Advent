cf0b05246686a4cb28ac5e92fc723ecd287ff0f9
frnce <francetag32@gmail.com>
2019-03-07 07:28:19 +0800

    0

---
+++ Advent/Assets/Script/EntityStats/CharacterStats.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Advent.Stats
+{
+    public class CharacterStats : MonoBehaviour
+    {
+        [Header("HP and ST")]
+        public Stat health;
+        public int currentHealth { get; set; }
+
+        public Stat stamina;
+        public int currentStamina { get; set; }
+
+        [Header("Attack and Def")]
+        public Stat physicalAttack;
+        public Stat magicalAttack;
+        public Stat defense;
+
+        [Header("Stats")]
+        public Stat strength;
+        public Stat dexterity;
+        public Stat intelligence;
+        public Stat vitality;
+        public Stat speed;
+
+        private void Awake()
+        {
+            currentHealth = health.GetValue();
+            currentStamina = stamina.GetValue();
+        }
+        public void TakeDamage(int damage)
+        {
+            damage -= defense.GetValue();
+            damage = Mathf.Clamp(damage, 0, int.MaxValue); //have room for improvements ,. ,balancing shits
+
+            currentHealth -= damage;
+            Debug.Log("Take Damage" + transform.name);
+
+            if (currentHealth <= 0)
+            {
+                Die();
+            }
+        }
+        public virtual void Die()
+        {
+            //Die in some way 
+            //meant to be overwritten
+            Debug.Log("Died");
+        }
+    }
+}
---
+++ Advent/Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Advent.Stats;
+
+namespace Advent.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        #region singleton
+        public static PlayerController instance;
+        private void Awake()
+        {
+            instance = this;
+        }
+        #endregion
+        PlayerStats stats;
+        float xMove, yMove;
+        Rigidbody2D rb2d;
+        // Start is called before the first frame update
+        void Start()
+        {
+            rb2d = GetComponent<Rigidbody2D>();
+            stats = GetComponent<PlayerStats>();
+        }
+        private void Update()
+        {
+            GetInput();
+        }
+        public void GetInput()
+        {
+            xMove = Input.GetAxisRaw("Horizontal");
+            yMove = Input.GetAxisRaw("Vertical");
+        }
+        private void FixedUpdate()
+        {
+            rb2d.velocity = new Vector2(xMove * stats.speed.GetValue(), yMove * stats.speed.GetValue());
+        }
+    }
+}
