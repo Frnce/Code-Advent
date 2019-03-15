9a7c9442ee08b53fea5ca98c743fc43add074b6e
frnce <francetag32@gmail.com>
2019-03-16 00:11:16 +0800

    fixed attack animation / Init spell system

---
+++ Advent/Assets/Script/CharacterSpells/Spells.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Advent.Spells
+{
+    [System.Serializable]
+    public class Spells
+    {
+        public int id;
+        public string name;
+        public string description;
+
+        public Spells(Spells spells)
+        {
+            id = spells.id;
+            name = spells.name;
+            description = spells.description;
+        }
+    }
+}
