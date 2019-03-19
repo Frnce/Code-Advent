c85d62c92a0c11dea02b0c8dd7aaf45d0545d2e3
frnce <francetag32@gmail.com>
2019-03-20 00:43:35 +0800

    Init from ARPG to roguelike

---
+++ Advent/Assets/Script/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Advent
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager instance = null;
+
+        [HideInInspector] public bool playersTurn = true;
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else if( instance != this)
+            {
+                Destroy(gameObject);
+            }
+
+            DontDestroyOnLoad(gameObject);
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void GameOver()
+        {
+            enabled = false;
+        }
+    }
+}
---
+++ Advent/Assets/Script/Player/Player.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Advent.Entities
+{
+    public class Player : EntityObject
+    {
+        GameManager gameManager;
+        protected override void Start()
+        {
+            gameManager = GameManager.instance;
+            base.Start();
+        }
+        private void OnDisable()
+        {
+            //gameManager.playerFoodPoints = food;
+        }
+        private void Update()
+        {
+            if (!gameManager.playersTurn)
+            {
+                return;
+            }
+
+            int horizontal = 0;
+            int vertical = 0;
+
+            horizontal = (int)Input.GetAxisRaw("Horizontal");
+            vertical = (int)Input.GetAxisRaw("Vertical");
+
+            if(horizontal != 0)
+            {
+                vertical = 0;
+            }
+            if(horizontal != 0 || vertical != 0)
+            {
+                AttemptMove<ChestScript>(horizontal, vertical); // TODO "ChestScript is temporary" change to proper function like wall or enemy;
+            }
+        }
+        protected override void AttemptMove<T>(int xDir, int yDir)
+        {
+            base.AttemptMove<T>(xDir, yDir);
+            RaycastHit2D hit;
+            CheckIfGameOver();
+            if (Move(xDir, yDir, out hit))
+            {
+                //Call RandomizeSfx of SoundManager to play the move sound, passing in two audio clips to choose from.
+            }
+            gameManager.playersTurn = false;
+        }
+        protected override void OnCantMove<T>(T component)
+        {
+            throw new System.NotImplementedException();
+        }
+        private void CheckIfGameOver()
+        {
+            int asd = 1; //TODO remove . Please fix to proper function
+            if (asd == 0)
+            {
+                gameManager.GameOver();
+            }
+        }
+    }
+}
