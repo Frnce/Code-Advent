3c5eb5d41f5e4da65b1268e45842a1ec92c4a7fe
frnce <francetag32@gmail.com>
2019-03-26 23:10:15 +0800

    Added Random generated Dungeon ala mystery Dungeon

---
+++ Advent/Assets/Script/Dungeon Generation/BoardCreator.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Advent;
+
+namespace Advent.Dungeons
+{
+    public class BoardCreator : MonoBehaviour
+    {
+        public enum TileType
+        {
+            WALL,
+            FLOOR,
+        }
+        public int columns = 100;
+        public int rows = 100;
+        public IntRange numOfRooms = new IntRange(15, 20);
+        public IntRange roomWidth = new IntRange(3, 10);
+        public IntRange roomHeight = new IntRange(3, 10);
+        public IntRange corridorLength = new IntRange(6, 10);
+        public GameObject[] floorTiles;
+        public GameObject[] wallTiles;
+        public GameObject[] outerWallTiles;
+        public GameObject player;
+        public GameObject GUI;
+
+        private TileType[][] tiles;
+        private Room[] rooms;
+        private Corridor[] corridors;
+        private GameObject boardHolder;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            boardHolder = new GameObject("BoardHolder");
+
+            SetupTilesArray();
+            CreateRoomsAndCorridors();
+
+            SetTilesValuesForRooms();
+            SetTilesValluesForCorridors();
+
+            InstantiateTiles();
+            InstantiateOuterWalls();
+
+            InstantiateUI();
+        }
+
+        private void SetupTilesArray()
+        {
+            tiles = new TileType[columns][];
+
+            for (int i = 0; i < tiles.Length; i++)
+            {
+                tiles[i] = new TileType[rows];
+            }
+        }
+
+        private void CreateRoomsAndCorridors()
+        {
+            rooms = new Room[numOfRooms.Random];
+            corridors = new Corridor[rooms.Length - 1];
+
+            rooms[0] = new Room();
+            corridors[0] = new Corridor();
+
+            rooms[0].SetupRoom(roomWidth, roomHeight, columns, rows);
+            corridors[0].SetupCorridor(rooms[0], corridorLength, roomWidth, roomHeight, columns, rows, true);
+
+            for (int i = 1; i < rooms.Length; i++)
+            {
+                rooms[i] = new Room();
+                rooms[i].SetupRoom(roomWidth, roomHeight, columns, rows, corridors[i - 1]);
+
+                if(i < corridors.Length)
+                {
+                    corridors[i] = new Corridor();
+                    corridors[i].SetupCorridor(rooms[i], corridorLength, roomWidth, roomHeight, columns, rows, false);
+                }
+
+                if (i == rooms.Length * 0.5f)
+                {
+                    Vector3 playerPos = new Vector3(rooms[i].xPosition, rooms[i].yPosition, 0);
+                    Instantiate(player, playerPos, Quaternion.identity);
+                }
+            }
+        }
+        private void SetTilesValuesForRooms()
+        {
+            for (int i = 0; i < rooms.Length; i++)
+            {
+                Room currentRoom = rooms[i];
+
+                for (int j = 0; j < currentRoom.roomWidth; j++)
+                {
+                    int xCoordinate = currentRoom.xPosition + j;
+
+                    for (int k = 0; k < currentRoom.roomHeight; k++)
+                    {
+                        int yCoordinate = currentRoom.yPosition + k;
+
+                        tiles[xCoordinate][yCoordinate] = TileType.FLOOR;
+                    }
+                }
+            }
+        }
+        private void SetTilesValluesForCorridors()
+        {
+            for (int i = 0; i < corridors.Length; i++)
+            {
+                Corridor currentCorridor = corridors[i];
+
+                for (int j = 0; j < currentCorridor.corridorLength; j++)
+                {
+                    int xCoordinate = currentCorridor.startXPosition;
+                    int yCoordinate = currentCorridor.startYPosition;
+
+                    switch (currentCorridor.direction)
+                    {
+                        case Direction.NORTH:
+                            yCoordinate += j;
+                            break;
+                        case Direction.EAST:
+                            xCoordinate += j;
+                            break;
+                        case Direction.SOUTH:
+                            yCoordinate -= j;
+                            break;
+                        case Direction.WEST:
+                            xCoordinate -= j;
+                            break;
+                    }
+
+                    tiles[xCoordinate][yCoordinate] = TileType.FLOOR;
+                }
+            }
+        }
+        private void InstantiateTiles()
+        {
+            for (int i = 0; i < tiles.Length; i++)
+            {
+                for (int j = 0; j < tiles[i].Length; j++)
+                {
+                    InstantiateFromArray(floorTiles, i, j);
+                    if (tiles[i][j] == TileType.WALL)
+                    {
+                        InstantiateFromArray(wallTiles, i, j);
+                    }
+                }
+            }
+        }
+        private void InstantiateOuterWalls()
+        {
+            float leftEdgeX = -1f;
+            float rightEdgeX = columns + 0f;
+            float bottomEdgeY = -1f;
+            float topEdgeY = rows + 0f;
+
+            InstantiateVerticalOuterWall(leftEdgeX, bottomEdgeY, topEdgeY);
+            InstantiateVerticalOuterWall(rightEdgeX, bottomEdgeY, topEdgeY);
+
+            InstantiateHorizontalOuterWall(leftEdgeX + 1f, rightEdgeX - 1f, bottomEdgeY);
+            InstantiateHorizontalOuterWall(leftEdgeX + 1f, rightEdgeX - 1f, topEdgeY);
+        }
+        private void InstantiateUI()
+        {
+            Instantiate(GUI);
+        }
+        private void InstantiateVerticalOuterWall(float xCoordinate, float startingY,float endingY)
+        {
+            float currentY = startingY;
+            while(currentY <= endingY)
+            {
+                InstantiateFromArray(outerWallTiles, xCoordinate, currentY);
+                currentY++;
+            }
+        }
+        private void InstantiateHorizontalOuterWall(float startingX, float endingX, float yCoord)
+        {
+            float currentX = startingX;
+            while(currentX <= endingX)
+            {
+                InstantiateFromArray(outerWallTiles, currentX, yCoord);
+                currentX++;
+            }
+        }
+        void InstantiateFromArray(GameObject[] prefabs, float xCoord, float yCoord)
+        {
+            // Create a random index for the array.
+            int randomIndex = Random.Range(0, prefabs.Length);
+
+            // The position to be instantiated at is based on the coordinates.
+            Vector3 position = new Vector3(xCoord, yCoord, 0f);
+
+            // Create an instance of the prefab from the random index of the array.
+            GameObject tileInstance = Instantiate(prefabs[randomIndex], position, Quaternion.identity) as GameObject;
+
+            // Set the tile's parent to the board holder.
+            tileInstance.transform.parent = boardHolder.transform;
+        }
+    }
+}
---
+++ Advent/Assets/Script/Dungeon Generation/Corridor.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Advent.Dungeons
+{
+    public enum Direction
+    {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST,
+    }
+
+    public class Corridor
+    {
+        public int startXPosition;
+        public int startYPosition;
+        public int corridorLength;
+        public Direction direction;
+
+        public int EndPositionX
+        {
+            get
+            {
+                if (direction == Direction.NORTH || direction == Direction.SOUTH)
+                {
+                    return startXPosition;
+                }
+                if(direction == Direction.EAST)
+                {
+                    return startXPosition + corridorLength - 1;
+                }
+                return startXPosition - corridorLength + 1;
+            }
+        }
+        public int EndPositionY
+        {
+            get
+            {
+                if(direction == Direction.EAST || direction == Direction.WEST)
+                {
+                    return startYPosition;
+                }
+                if(direction == Direction.NORTH)
+                {
+                    return startYPosition + corridorLength - 1;
+                }
+                return startYPosition - corridorLength + 1;
+            }
+        }
+        public void SetupCorridor(Room room, IntRange length, IntRange roomWidth, IntRange roomHeight, int columns, int rows, bool firstCorridor)
+        {
+            direction = (Direction)Random.Range(0, 4);
+
+            Direction oppositeDirection = (Direction)(((int)room.enteringCorridor + 2) % 4);
+
+            if (!firstCorridor && direction == oppositeDirection)
+            {
+                int directionInt = (int)direction;
+                directionInt++;
+                directionInt = directionInt % 4;
+                direction = (Direction)directionInt;
+            }
+            corridorLength = length.Random;
+
+            int maxLength = length.m_Max;
+
+            switch (direction)
+            {
+                case Direction.NORTH:
+                    startXPosition = Random.Range(room.xPosition, room.xPosition + room.roomWidth - 1);
+                    startYPosition = room.yPosition + room.roomHeight;
+
+                    maxLength = rows - startYPosition - room.roomHeight;
+                    break;
+                case Direction.EAST:
+                    startXPosition = room.xPosition + room.roomWidth;
+                    startYPosition = Random.Range(room.yPosition, room.yPosition + room.roomHeight - 1);
+                    maxLength = columns - startXPosition - roomWidth.m_Min;
+                    break;
+                case Direction.SOUTH:
+                    startXPosition = Random.Range(room.xPosition, room.xPosition + room.roomWidth);
+                    startYPosition = room.yPosition;
+                    maxLength = startYPosition - roomHeight.m_Min;
+                    break;
+                case Direction.WEST:
+                    startXPosition = room.xPosition;
+                    startYPosition = Random.Range(room.yPosition, room.yPosition + room.roomHeight);
+                    maxLength = startXPosition - roomWidth.m_Min;
+                    break;
+                default:
+                    break;
+            }
+            corridorLength = Mathf.Clamp(corridorLength, 1, maxLength);
+        }
+    }
+}
