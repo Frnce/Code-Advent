2a8bd0ffe40d36ba3dfcaa0e7e049ca43e1dbf86
Frnce <francetag32@gmail.com>
2019-03-27 17:21:07 +0800

    Added Level Loader

---
+++ Advent/Assets/Script/LevelLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Advent
+{
+    public class LevelLoader : MonoBehaviour
+    {
+        public GameObject gameManager;          //GameManager prefab to instantiate.
+        void Awake()
+        {
+            if (GameManager.instance == null)
+            {
+                Instantiate(gameManager);
+            }
+        }
+    }
+}
