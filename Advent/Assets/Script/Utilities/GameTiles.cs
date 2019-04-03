7ca7620271fe91017476b760912fd07f76c72a7c
frnce <francetag32@gmail.com>
2019-04-04 01:42:21 +0800

    Can Now follow the If a Tile is Occupied by the player

--- Advent/Assets/Script/Utilities/GameTiles.cs
+++ Advent/Assets/Script/Utilities/GameTiles.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -8,9 +9,11 @@ namespace Advent.Utilities
     public class GameTiles : MonoBehaviour
     {
         public static GameTiles instance;
-        public Tilemap Tilemap;
+        public Tilemap tilemap;
 
         public Dictionary<Vector3, WorldTile> tiles;
+        public delegate void OnTileStatusChange();
+        public OnTileStatusChange onTileChange;
 
         private void Awake()
         {
@@ -23,30 +26,56 @@ namespace Advent.Utilities
                 Destroy(gameObject);
             }
         }
-
+        private void Start()
+        {
+            onTileChange += SeekTileStatus;
+        }
+        public void SeekTileStatus()
+        {
+            foreach (KeyValuePair<Vector3,WorldTile> item in tiles)
+            {
+                tilemap.SetTileFlags(item.Value.LocalPlace, TileFlags.None);
+                switch (item.Value.tileStatus)
+                {
+                    case TileStatus.EMPTY:
+                        tilemap.SetColor(item.Value.LocalPlace, Color.white);
+                        break;
+                    case TileStatus.PLAYER:
+                        tilemap.SetColor(item.Value.LocalPlace, Color.green);
+                        break;
+                    case TileStatus.ENEMY:
+                        tilemap.SetColor(item.Value.LocalPlace, Color.red);
+                        break;
+                }
+            }
+        }
         public void GetWorldTiles()
         {
             tiles = new Dictionary<Vector3, WorldTile>();
-            foreach (Vector3Int pos in Tilemap.cellBounds.allPositionsWithin)
+            foreach (Vector3Int pos in tilemap.cellBounds.allPositionsWithin)
             {
                 var localPlace = new Vector3Int(pos.x, pos.y, pos.z);
 
-                if (!Tilemap.HasTile(localPlace))
+                if (!tilemap.HasTile(localPlace))
                 {
                     continue;
                 }
                 var tile = new WorldTile
                 {
                     LocalPlace = localPlace,
-                    WorldLocation = Tilemap.CellToWorld(localPlace),
-                    TileBase = Tilemap.GetTile(localPlace),
-                    TilemapMember = Tilemap,
+                    WorldLocation = tilemap.CellToWorld(localPlace),
+                    TileBase = tilemap.GetTile(localPlace),
+                    TilemapMember = tilemap,
                     Name = localPlace.x + "," + localPlace.y,
                     Cost = 1,
                     tileStatus = TileStatus.EMPTY
                 };
                 tiles.Add(tile.WorldLocation, tile);
             }
+        }
+        public void SetTileStatus(WorldTile currentTile,TileStatus status)
+        {
+            currentTile.tileStatus = status;
         }
     }
 }
